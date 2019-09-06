8a11cd58022e46eacd64c4c015964f341b83f2c5
SamarV-121 <samarvispute121@gmail.com>
2019-09-06 10:01:47 +0000

    RMX1801: Cleanup

--- RMX1801/RMX1801-vendor.mk
+++ RMX1801/RMX1801-vendor.mk
@@ -17,15 +17,6 @@
 
 PRODUCT_COPY_FILES += \
     vendor/oppo/RMX1801/proprietary/bin/dpmd:system/bin/dpmd \
-    vendor/oppo/RMX1801/proprietary/etc/camera/ashdr/ncf_pack.ncf:system/etc/camera/ashdr/ncf_pack.ncf \
-    vendor/oppo/RMX1801/proprietary/etc/camera/cp/CPCalibrationData.bin:system/etc/camera/cp/CPCalibrationData.bin \
-    vendor/oppo/RMX1801/proprietary/etc/camera/cp/FusionCalibrationParams.bin:system/etc/camera/cp/FusionCalibrationParams.bin \
-    vendor/oppo/RMX1801/proprietary/etc/camera/cp/SystemParamsBokeh.bin:system/etc/camera/cp/SystemParamsBokeh.bin \
-    vendor/oppo/RMX1801/proprietary/etc/camera/cp/SystemParamsFusion.bin:system/etc/camera/cp/SystemParamsFusion.bin \
-    vendor/oppo/RMX1801/proprietary/etc/camera/faceTpl0.rgb32:system/etc/camera/faceTpl0.rgb32 \
-    vendor/oppo/RMX1801/proprietary/etc/camera/faceTpl1.rgb32:system/etc/camera/faceTpl1.rgb32 \
-    vendor/oppo/RMX1801/proprietary/etc/camera/gasryuv.model:system/etc/camera/gasryuv.model \
-    vendor/oppo/RMX1801/proprietary/etc/camera/skinSeg.model:system/etc/camera/skinSeg.model \
     vendor/oppo/RMX1801/proprietary/etc/dpm/dpm.conf:system/etc/dpm/dpm.conf \
     vendor/oppo/RMX1801/proprietary/etc/firmware/cpp_firmware_v1_10_0.fw:system/etc/firmware/cpp_firmware_v1_10_0.fw \
     vendor/oppo/RMX1801/proprietary/etc/firmware/cpp_firmware_v1_12_0.fw:system/etc/firmware/cpp_firmware_v1_12_0.fw \
@@ -51,43 +42,6 @@ PRODUCT_COPY_FILES += \
     vendor/oppo/RMX1801/proprietary/etc/permissions/qti_libpermissions.xml:system/etc/permissions/qti_libpermissions.xml \
     vendor/oppo/RMX1801/proprietary/etc/permissions/qti_permissions.xml:system/etc/permissions/qti_permissions.xml \
     vendor/oppo/RMX1801/proprietary/etc/permissions/telephonyservice.xml:system/etc/permissions/telephonyservice.xml \
-    vendor/oppo/RMX1801/proprietary/etc/vstab_configs/GyroOffset_db.config:system/etc/vstab_configs/GyroOffset_db.config \
-    vendor/oppo/RMX1801/proprietary/etc/vstab_configs/fsync_db.config:system/etc/vstab_configs/fsync_db.config \
-    vendor/oppo/RMX1801/proprietary/etc/vstab_configs/vstab_db_1080p_preview.config:system/etc/vstab_configs/vstab_db_1080p_preview.config \
-    vendor/oppo/RMX1801/proprietary/etc/vstab_configs/vstab_db_1080p_preview_0_imx519_bmi.config:system/etc/vstab_configs/vstab_db_1080p_preview_0_imx519_bmi.config \
-    vendor/oppo/RMX1801/proprietary/etc/vstab_configs/vstab_db_1080p_preview_0_imx519_lsm.config:system/etc/vstab_configs/vstab_db_1080p_preview_0_imx519_lsm.config \
-    vendor/oppo/RMX1801/proprietary/etc/vstab_configs/vstab_db_1080p_video_0.config:system/etc/vstab_configs/vstab_db_1080p_video_0.config \
-    vendor/oppo/RMX1801/proprietary/etc/vstab_configs/vstab_db_1080p_video_0_imx519_bmi.config:system/etc/vstab_configs/vstab_db_1080p_video_0_imx519_bmi.config \
-    vendor/oppo/RMX1801/proprietary/etc/vstab_configs/vstab_db_1080p_video_0_imx519_lsm.config:system/etc/vstab_configs/vstab_db_1080p_video_0_imx519_lsm.config \
-    vendor/oppo/RMX1801/proprietary/etc/vstab_configs/vstab_db_1080p_video_0_s5k2p7sq.config:system/etc/vstab_configs/vstab_db_1080p_video_0_s5k2p7sq.config \
-    vendor/oppo/RMX1801/proprietary/etc/vstab_configs/vstab_db_1080p_video_0_s5k2p7sq_sfov.config:system/etc/vstab_configs/vstab_db_1080p_video_0_s5k2p7sq_sfov.config \
-    vendor/oppo/RMX1801/proprietary/etc/vstab_configs/vstab_db_1080p_video_0_s5k2p7sq_sfov_bmi.config:system/etc/vstab_configs/vstab_db_1080p_video_0_s5k2p7sq_sfov_bmi.config \
-    vendor/oppo/RMX1801/proprietary/etc/vstab_configs/vstab_db_1080p_video_0_s5k2p7sq_sfov_main.config:system/etc/vstab_configs/vstab_db_1080p_video_0_s5k2p7sq_sfov_main.config \
-    vendor/oppo/RMX1801/proprietary/etc/vstab_configs/vstab_db_1080p_video_0_sfov.config:system/etc/vstab_configs/vstab_db_1080p_video_0_sfov.config \
-    vendor/oppo/RMX1801/proprietary/etc/vstab_configs/vstab_db_1080p_video_0_sfov_lsm.config:system/etc/vstab_configs/vstab_db_1080p_video_0_sfov_lsm.config \
-    vendor/oppo/RMX1801/proprietary/etc/vstab_configs/vstab_db_1080p_video_0_sfov_main.config:system/etc/vstab_configs/vstab_db_1080p_video_0_sfov_main.config \
-    vendor/oppo/RMX1801/proprietary/etc/vstab_configs/vstab_db_1080p_video_1.config:system/etc/vstab_configs/vstab_db_1080p_video_1.config \
-    vendor/oppo/RMX1801/proprietary/etc/vstab_configs/vstab_db_1350p_video_0.config:system/etc/vstab_configs/vstab_db_1350p_video_0.config \
-    vendor/oppo/RMX1801/proprietary/etc/vstab_configs/vstab_db_1350p_video_0_s5k2p7sq.config:system/etc/vstab_configs/vstab_db_1350p_video_0_s5k2p7sq.config \
-    vendor/oppo/RMX1801/proprietary/etc/vstab_configs/vstab_db_4k_preview.config:system/etc/vstab_configs/vstab_db_4k_preview.config \
-    vendor/oppo/RMX1801/proprietary/etc/vstab_configs/vstab_db_4k_video_0.config:system/etc/vstab_configs/vstab_db_4k_video_0.config \
-    vendor/oppo/RMX1801/proprietary/etc/vstab_configs/vstab_db_4k_video_1.config:system/etc/vstab_configs/vstab_db_4k_video_1.config \
-    vendor/oppo/RMX1801/proprietary/etc/vstab_configs/vstab_db_720p_preview.config:system/etc/vstab_configs/vstab_db_720p_preview.config \
-    vendor/oppo/RMX1801/proprietary/etc/vstab_configs/vstab_db_720p_preview_0_imx519_bmi.config:system/etc/vstab_configs/vstab_db_720p_preview_0_imx519_bmi.config \
-    vendor/oppo/RMX1801/proprietary/etc/vstab_configs/vstab_db_720p_preview_0_imx519_lsm.config:system/etc/vstab_configs/vstab_db_720p_preview_0_imx519_lsm.config \
-    vendor/oppo/RMX1801/proprietary/etc/vstab_configs/vstab_db_720p_video_0.config:system/etc/vstab_configs/vstab_db_720p_video_0.config \
-    vendor/oppo/RMX1801/proprietary/etc/vstab_configs/vstab_db_720p_video_0_imx519_bmi.config:system/etc/vstab_configs/vstab_db_720p_video_0_imx519_bmi.config \
-    vendor/oppo/RMX1801/proprietary/etc/vstab_configs/vstab_db_720p_video_0_imx519_lsm.config:system/etc/vstab_configs/vstab_db_720p_video_0_imx519_lsm.config \
-    vendor/oppo/RMX1801/proprietary/etc/vstab_configs/vstab_db_720p_video_0_s5k2p7sq.config:system/etc/vstab_configs/vstab_db_720p_video_0_s5k2p7sq.config \
-    vendor/oppo/RMX1801/proprietary/etc/vstab_configs/vstab_db_720p_video_0_s5k2p7sq_sfov.config:system/etc/vstab_configs/vstab_db_720p_video_0_s5k2p7sq_sfov.config \
-    vendor/oppo/RMX1801/proprietary/etc/vstab_configs/vstab_db_720p_video_0_s5k2p7sq_sfov_bmi.config:system/etc/vstab_configs/vstab_db_720p_video_0_s5k2p7sq_sfov_bmi.config \
-    vendor/oppo/RMX1801/proprietary/etc/vstab_configs/vstab_db_720p_video_0_s5k2p7sq_sfov_main.config:system/etc/vstab_configs/vstab_db_720p_video_0_s5k2p7sq_sfov_main.config \
-    vendor/oppo/RMX1801/proprietary/etc/vstab_configs/vstab_db_720p_video_0_sfov.config:system/etc/vstab_configs/vstab_db_720p_video_0_sfov.config \
-    vendor/oppo/RMX1801/proprietary/etc/vstab_configs/vstab_db_720p_video_0_sfov_lsm.config:system/etc/vstab_configs/vstab_db_720p_video_0_sfov_lsm.config \
-    vendor/oppo/RMX1801/proprietary/etc/vstab_configs/vstab_db_720p_video_0_sfov_main.config:system/etc/vstab_configs/vstab_db_720p_video_0_sfov_main.config \
-    vendor/oppo/RMX1801/proprietary/etc/vstab_configs/vstab_db_720p_video_1.config:system/etc/vstab_configs/vstab_db_720p_video_1.config \
-    vendor/oppo/RMX1801/proprietary/etc/vstab_configs/vstab_db_900p_video_0.config:system/etc/vstab_configs/vstab_db_900p_video_0.config \
-    vendor/oppo/RMX1801/proprietary/etc/vstab_configs/vstab_db_900p_video_0_s5k2p7sq.config:system/etc/vstab_configs/vstab_db_900p_video_0_s5k2p7sq.config \
     vendor/oppo/RMX1801/proprietary/framework/QtiTelephonyServicelibrary.jar:system/framework/QtiTelephonyServicelibrary.jar \
     vendor/oppo/RMX1801/proprietary/framework/cneapiclient.jar:system/framework/cneapiclient.jar \
     vendor/oppo/RMX1801/proprietary/framework/com.qti.dpmframework.jar:system/framework/com.qti.dpmframework.jar \
@@ -115,11 +69,8 @@ PRODUCT_COPY_FILES += \
     vendor/oppo/RMX1801/proprietary/framework/vendor.qti.hardware.fingerprint-V1.0-java.jar:system/framework/vendor.qti.hardware.fingerprint-V1.0-java.jar \
     vendor/oppo/RMX1801/proprietary/framework/vendor.qti.ims.callinfo-V1.0-java.jar:system/framework/vendor.qti.ims.callinfo-V1.0-java.jar \
     vendor/oppo/RMX1801/proprietary/framework/vendor.qti.ims.rcsconfig-V1.0-java.jar:system/framework/vendor.qti.ims.rcsconfig-V1.0-java.jar \
-    vendor/oppo/RMX1801/proprietary/lib/android.frameworks.fingerprintservice@1.0.so:system/lib/android.frameworks.fingerprintservice@1.0.so \
-    vendor/oppo/RMX1801/proprietary/lib/android.hardware.biometrics.fingerprint@2.1.so:system/lib/android.hardware.biometrics.fingerprint@2.1.so \
     vendor/oppo/RMX1801/proprietary/lib/vendor.oppo.hardware.biometrics.fingerprint@2.1.so:system/lib/vendor.oppo.hardware.biometrics.fingerprint@2.1.so \
-    vendor/oppo/RMX1801/proprietary/lib64/android.frameworks.fingerprintservice@1.0.so:system/lib64/android.frameworks.fingerprintservice@1.0.so \
-    vendor/oppo/RMX1801/proprietary/lib64/android.hardware.biometrics.fingerprint@2.1.so:system/lib64/android.hardware.biometrics.fingerprint@2.1.so \
+    vendor/oppo/RMX1801/proprietary/lib/vendor.qti.hardware.fingerprint@1.0.so:system/lib/vendor.qti.hardware.fingerprint@1.0.so \
     vendor/oppo/RMX1801/proprietary/lib64/com.qualcomm.qti.dpm.api@1.0.so:system/lib64/com.qualcomm.qti.dpm.api@1.0.so \
     vendor/oppo/RMX1801/proprietary/lib64/lib-imsvideocodec.so:system/lib64/lib-imsvideocodec.so \
     vendor/oppo/RMX1801/proprietary/lib64/lib-imsvt.so:system/lib64/lib-imsvt.so \
@@ -134,6 +85,7 @@ PRODUCT_COPY_FILES += \
     vendor/oppo/RMX1801/proprietary/lib64/libimsmedia_jni.so:system/lib64/libimsmedia_jni.so \
     vendor/oppo/RMX1801/proprietary/lib64/librcc.so:system/lib64/librcc.so \
     vendor/oppo/RMX1801/proprietary/lib64/vendor.oppo.hardware.biometrics.fingerprint@2.1.so:system/lib64/vendor.oppo.hardware.biometrics.fingerprint@2.1.so \
+    vendor/oppo/RMX1801/proprietary/lib64/vendor.qti.hardware.fingerprint@1.0.so:system/lib64/vendor.qti.hardware.fingerprint@1.0.so \
     vendor/oppo/RMX1801/proprietary/lib64/vendor.qti.imsrtpservice@1.0.so:system/lib64/vendor.qti.imsrtpservice@1.0.so
 
 PRODUCT_PACKAGES += \
